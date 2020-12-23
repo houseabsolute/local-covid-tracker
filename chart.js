e952bef6ff1d32669047ad4e366c87f11f50f6d2
Dave Rolsky <autarch@urth.org>
2020-12-23 15:46:22 -0600

    Add daily details to chart

--- chart.js
+++ chart.js
@@ -1,7 +1,7 @@
 function makeChart() {
     var margin = {
             top: 10,
-            right: 10,
+            right: 30,
             bottom: 30,
             left: 50,
         },
@@ -16,11 +16,23 @@ function makeChart() {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.json("summary.json").then(function (data) {
-        var tp = d3.timeParse("%Y-%m-%d");
+    d3.json("summary.json").then((data) => {
+        var tp = d3.utcParse("%Y-%m-%d");
+        var keyF = d3.utcFormat("%Y-%m-%d");
 
+        var byDay = {};
         data = data.filter((d) => +d.diff > 0);
-        data.forEach((d) => (d.day = tp(d.day)));
+        data.forEach((d) => {
+            d.day = tp(d.day);
+            d.sub_province = d.sub_province.replaceAll(/^\*\*|\*\*$/g, "");
+            var key = keyF(d.day);
+            if (typeof byDay[key] === "undefined") {
+                byDay[key] = {};
+            }
+            byDay[key][d.sub_province] = d;
+        });
+
+        var firstDay = byDay[Object.keys(byDay).sort()[0]];
 
         var x = d3
             .scaleTime()
@@ -50,13 +62,13 @@ function makeChart() {
             .attr("fill", "none")
             .attr("stroke", (d) => color(d.key))
             .attr("stroke-width", 1)
-            .attr("d", function (d) {
-                return d3
+            .attr("d", (d) =>
+                d3
                     .line()
                     .curve(d3.curveBasis)
                     .x((d) => x(d.day))
-                    .y((d) => y(+d.diff))(d.values);
-            });
+                    .y((d) => y(+d.diff))(d.values)
+            );
 
         var sdas = d3
             .nest()
@@ -68,25 +80,27 @@ function makeChart() {
             .data(sdas)
             .enter()
             .append("path")
+            .attr("class", "line")
             .attr("fill", "none")
             .attr("stroke", (d) => sdaColor(d.key))
             .attr("stroke-width", 3)
-            .attr("d", function (d) {
-                return d3
+            .attr("d", (d) =>
+                d3
                     .line()
                     .curve(d3.curveBasis)
                     .x((d) => x(d.day))
-                    .y((d) => y(+d.seven_day_average))(d.values);
-            });
+                    .y((d) => y(+d.seven_day_average))(d.values)
+            );
 
         var size = 14;
-        var offset = 70;
+        var xOffset = 70;
+
         svg.selectAll("legend-dots")
             .data(keys)
             .enter()
             .append("rect")
-            .attr("x", offset)
-            .attr("y", (_, i) => offset + i * (size + 5))
+            .attr("x", xOffset)
+            .attr("y", (_, i) => xOffset + i * (size + 5))
             .attr("width", size)
             .attr("height", size)
             .style("fill", (d) => color(d));
@@ -95,11 +109,81 @@ function makeChart() {
             .data(keys)
             .enter()
             .append("text")
-            .attr("x", offset + 3 + size * 1.2)
-            .attr("y", (_, i) => offset + 2 + i * (size + 5) + size / 2)
+            .attr("x", xOffset + 3 + size * 1.2)
+            .attr("y", (_, i) => xOffset + 2 + i * (size + 5) + size / 2)
             .style("fill", (d) => color(d))
-            .text((d) => d.replaceAll(/^\*\*|\*\*$/g, ""))
+            .text((d) => d)
             .attr("text-anchor", "left")
             .style("alignment-baseline", "middle");
+
+        var mouseG = svg.append("g").attr("class", "mouse-over-effects");
+
+        // This is the black vertical line to follow the mouse.
+        mouseG
+            .append("path")
+            .attr("class", "mouse-line")
+            .style("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", "0");
+
+        // We can't get mouse events on a g element so we add this.
+        mouseG
+            .append("svg:rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("fill", "none")
+            .attr("pointer-events", "all")
+            .on("mouseout", () => {
+                d3.select(".mouse-line").style("opacity", "0");
+            })
+            .on("mouseover", () => {
+                d3.select(".mouse-line").style("opacity", "1");
+                d3.select("#details").text("");
+            })
+            // We need to use function and not an arrow to get the right
+            // "this".
+            .on("mousemove", function () {
+                var mouse = d3.mouse(this);
+                d3.select(".mouse-line").attr("d", () => {
+                    var d = "M" + mouse[0] + "," + height;
+                    d += " " + mouse[0] + "," + 0;
+                    return d;
+                });
+
+                var key = x.invert(mouse[0]);
+                if (key.getHours() > 12) {
+                    key.setDate(key.getDate() + 1);
+                }
+                key = keyF(new Date(key.toDateString()));
+                var thisDay = byDay[key];
+                // 2020-11-26 is missing data.
+                if (typeof thisDay == "undefined") {
+                    return;
+                }
+
+                d3.selectAll("#details > *").remove();
+                var details = d3.select("#details");
+                details.append("h2").text(key);
+
+                var table = details.append("table");
+                var head = table.append("tr");
+                head.append("th").text("");
+                head.append("th").text("New cases");
+
+                var firstDate = firstDay[Object.keys(firstDay)[0]].day;
+                head.append("th").text("Change vs " + keyF(firstDate));
+
+                var keys = Object.keys(thisDay);
+                keys.sort();
+                keys.forEach((k) => {
+                    var tr = table.append("tr");
+                    tr.append("td").text(k);
+                    tr.append("td").attr("class", "diff").text(thisDay[k].diff);
+                    var mult = Math.round((thisDay[k].diff / firstDay[k].diff) * 10) / 10;
+                    tr.append("td")
+                        .attr("class", "multiply")
+                        .text(mult + "x");
+                });
+            });
     });
 }

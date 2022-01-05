bdc77c2b7208bcc7bcd197be2bf10608733a3839
Dave Rolsky <autarch@urth.org>
2022-01-05 09:43:44 -0600

    Turn this site off
    Something is getting weird with the data. Hennepin was showing nearly as many
    cases as the state, and the total of the nearby totals was much more than the
    state as a whole.

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,4 @@ set -x
 
 mkdir -p deploy
 
-ID=$(
-    curl \
-        --header "Accept: application/vnd.github.v3+json" \
-        -u "autarch:$GITHUB_TOKEN" \
-        https://api.github.com/repos/houseabsolute/local-covid-tracker/actions/artifacts | \
-        jq '.artifacts[0].id'
-)
-
-URL=$(
-curl \
-    --head \
-    -u "autarch:$GITHUB_TOKEN" \
-    https://api.github.com/repos/houseabsolute/local-covid-tracker/actions/artifacts/$ID/zip | \
-    grep -Fi "Location: " | \
-    sed "s/[Ll]ocation: /url=/"
-)
-
-echo "$URL" | \
-    curl -K -  | \
-    zcat > ./deploy/summary.json
-
-jq . ./deploy/summary.json
-
-cp chart.js index.html ./deploy/
+cp index.html ./deploy/
